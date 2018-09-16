701bcb63513047947404f8eed763fc06959d97e4
Оксана Ткаченко <oksana@otbook.local>
2018-09-16 09:25:42 +0300

    added percent to skills

--- src/index.js
+++ src/index.js
@@ -1,21 +1,15 @@
 import 'bootstrap';
 import $ from 'jquery';
 import circleProgress from 'jquery-circle-progress';
-/*
-$(document).ready(function () {
-  $('.carousel').carousel({
-    interval: 2000
-  });
-
-  $('.first.circle').circleProgress({
-    value: 0.6
-  }).on('circle-animation-progress', function(event, progress) {
-    $(this).find('strong').html(Math.round(100 * progress) + '<i>%</i>');
-  });
-});
-*/
 
 $(document).ready(function() {
+
+  /* slider */
+  $('.carousel').carousel({
+    interval: 3000
+  });
+
+  /* percent */
   $('.first.circle').circleProgress({
     value: 0.85,
     size: 140,
@@ -24,7 +18,43 @@ $(document).ready(function() {
     lineCap:"round",
     emptyFill:"rgba(255, 255, 255, 0.15)",
     fill: {
-      gradient: ["#fff", "#fff"]
+      gradient: ["#ffa502", "#ffa502"]
+    }
+  });
+
+  $('.second.circle').circleProgress({
+    value: 0.74,
+    size: 140,
+    startAngle:-Math.PI/2,
+    thickness:6,
+    lineCap:"round",
+    emptyFill:"rgba(255, 255, 255, 0.15)",
+    fill: {
+      gradient: ["#ffa502", "#ffa502"]
+    }
+  });
+
+  $('.third.circle').circleProgress({
+    value: 0.92,
+    size: 140,
+    startAngle:-Math.PI/2,
+    thickness:6,
+    lineCap:"round",
+    emptyFill:"rgba(255, 255, 255, 0.15)",
+    fill: {
+      gradient: ["#ffa502", "#ffa502"]
+    }
+  });
+
+  $('.fourth.circle').circleProgress({
+    value: 0.57,
+    size: 140,
+    startAngle:-Math.PI/2,
+    thickness:6,
+    lineCap:"round",
+    emptyFill:"rgba(255, 255, 255, 0.15)",
+    fill: {
+      gradient: ["#ffa502", "#ffa502"]
     }
   });
 });
